63b015b9a248ec78398c77c7c641f4d201be79b6
AdryGA07 <adrianlisto14@gmail.com>
2020-07-08 19:05:38 +0200

    Add Cube Controller

---
+++ Assets/CubeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public float speed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+      if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+    }
+}
--- Assets/TestConsole.cs
+++ Assets/TestConsole.cs
@@ -46,6 +46,37 @@ public class TestConsole : MonoBehaviour
         Debug.Log(name);
 
 
+        // Operadores
+
+        // Aritmeticos
+        int a = 2;
+        int b = 5;
+        int c = a + b;  // 7  SUMA
+
+        int dd = a * b;  // 10 MULTIPLICACION
+        int e = a / b;  // DIVISION
+        int f = a - b;  // -3
+
+        // Lógicos
+        // AND
+        bool g = true && true;   // true    VERDADERO Y VERDADERO = VERDADERO  1 * 1
+        bool g2 = true && false;   // false    VERDADERO Y FALSO = FALSO       1 * 0
+        bool g3 = false && true;   // false    FALSO Y VERDADERO = FALSO       0 * 1
+        bool g4 = false && false;  // false    FALSO Y FALSO = FALSO           0 * 0
+
+        // OR
+        bool h = true || true;   // true    VERDADERO O VERDADERO = VERDADERO   1 + 1 = 1
+        bool h2 = true ||  false;   // true    VERDADERO O FALSO = VERDADERO     1 + 0 = 1
+        bool h3 = false || true;   // true    FALSO O VERDADERO = VERDADERO     0 + 1 = 1
+        bool h4 = false || false;  // false    FALSO O FALSO = FALSO            0 + 0 = 0
+
+        // XOR
+        bool i = true ^ true;   // true    VERDADERO O VERDADERO = VERDADERO
+        bool i2 = true ^ false;   // false    VERDADERO O FALSO = FALSO 
+        bool i3 = false ^ true;   // false    FALSO O VERDADERO = FALSO
+        bool i4 = false ^ false;  // true    FALSO O FALSO = VERDADERO
+
+
 
     }
 }
